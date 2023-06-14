5dd344f58adeee336d3a686ac8dd2bdba690c0cb
Jasim Iqbal <jasimali045@gmail.com>
2023-06-14 11:47:47 +0500

    issue sending

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ $(window).on('load', function(){
     $('#status').delay(500).fadeOut(); 
     $('#preloader').delay(500).fadeOut(); 
 });
+/*
+============================================
+                 Team 
+============================================
+*/
+$(function()
+{
+$("#team-member").owlCarousel({
+    item:2,
+    autoplay:true,
+    smartSpeed:700,
+    loop:true,
+    autoplayHoverPause:true,
+    nav:true,
+    dots:false,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+
+});
+});
